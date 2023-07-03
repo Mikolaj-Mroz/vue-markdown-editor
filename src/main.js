a4680c7029f0a3b5c9a05ac0126c2221eaf3ca4f
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-03 10:09:40 +0200

    init project

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    iconfont: 'mdi',
+    aliases,
+    defaultSet: 'mdi',
+    sets: {
+      mdi
+    }
+  }
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(vuetify)
+
+app.mount('#app')
